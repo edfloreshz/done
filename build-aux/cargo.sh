e4d59554278c1fa07705e6b9e02f51492c905d12
Eduardo Flores <edfloreshz@gmail.com>
2022-05-06 15:25:03 -0700

    Meson used as build system

--- build-aux/cargo.sh
+++ build-aux/cargo.sh
@@ -12,13 +12,11 @@ export APP_BIN="$5"
 if [ $BUILDTYPE = "release" ]
 then
     echo "RELEASE MODE"
-    cargo build --manifest-path \
-        "$MESON_SOURCE_ROOT"/Cargo.toml --release && \
-        cp "$CARGO_TARGET_DIR"/release/"$APP_BIN" "$OUTPUT"
+    cargo build --manifest-path "$MESON_SOURCE_ROOT"/Cargo.toml --release --verbose && \
+    	cp "$CARGO_TARGET_DIR"/release/"$APP_BIN" "$OUTPUT"
 else
     echo "DEBUG MODE"
-    cargo build --manifest-path \
-        "$MESON_SOURCE_ROOT"/Cargo.toml && \
+    cargo build --manifest-path "$MESON_SOURCE_ROOT"/Cargo.toml --verbose && \
         cp "$CARGO_TARGET_DIR"/debug/"$APP_BIN" "$OUTPUT"
 fi
 
---
+++ src/const.rs
@@ -0,0 +1,4 @@
+pub static VERSION: &str = "0.1.0";
+pub static GETTEXT_PACKAGE: &str = "doable";
+pub static LOCALEDIR: &str = "/app/share/locale";
+pub static PKGDATADIR: &str = "/app/share/doable";
