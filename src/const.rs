b7b68d767de1cda4c90f0d2b41ffa343f8395242
Eduardo Flores <edfloreshz@gmail.com>
2022-05-06 16:37:23 -0700

    Implemented meson

--- src/const.rs
+++ src/const.rs
@@ -1,4 +1,4 @@
 pub static VERSION: &str = "0.1.0";
 pub static GETTEXT_PACKAGE: &str = "doable";
-pub static LOCALEDIR: &str = "/app/share/locale";
-pub static PKGDATADIR: &str = "/app/share/doable";
+pub static LOCALEDIR: &str = "/home/eduardo/.var/app/org.gnome.Builder/cache/gnome-builder/install/doable/host/share/locale";
+pub static PKGDATADIR: &str = "/home/eduardo/.var/app/org.gnome.Builder/cache/gnome-builder/install/doable/host/share/doable";
