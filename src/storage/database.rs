788e3d6968de614452113a263809222923c80287
Abraham Toriz <categulario@gmail.com>
2022-05-06 09:28:23 +0800

    only call dirs if necessary and join as path

--- src/storage/database.rs
+++ src/storage/database.rs
@@ -8,14 +8,10 @@ pub struct DatabaseConnection;
 impl DatabaseConnection {
     pub fn establish_connection() -> SqliteConnection {
         dotenv().ok();
-        let db = format!(
-            "{}/do/org.devloop.Do.db",
-            dirs::data_dir().unwrap().display()
-        );
-        let database_url = match env::var("DATABASE_URL") {
-            Ok(url) => url,
-            Err(_) => db,
-        };
+
+        let database_url = env::var("DATABASE_URL")
+            .unwrap_or_else(|_| dirs::data_dir().unwrap().join("do/org.devloop.Do.db").display().to_string());
+
         SqliteConnection::establish(&database_url)
             .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
     }
