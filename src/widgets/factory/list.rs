3f313a1a4d101d1072b9aea3b2750cd5fa932f90
Eduardo Flores <edfloreshz@gmail.com>
2022-05-15 19:10:06 -0700

    Migrated to macros-0.5

--- src/widgets/factory/list.rs
+++ src/widgets/factory/list.rs
@@ -1,8 +1,8 @@
-use relm4::{Sender, view};
 use relm4::factory::{DynamicIndex, FactoryComponent};
+use relm4::Sender;
 
 use crate::gtk;
-use crate::gtk::prelude::{BoxExt, OrientableExt, WidgetExt};
+use crate::gtk::prelude::{OrientableExt, WidgetExt};
 use crate::models::list::List;
 use crate::widgets::component::sidebar::SidebarInput;
 
@@ -27,20 +27,45 @@ pub enum ListOutput {
     RemoveList(DynamicIndex),
 }
 
-pub struct ListWidgets {
-    icon: gtk::Image,
-    name: gtk::Label,
-    count: gtk::Label,
-}
-
+#[relm4::factory(pub)]
 impl FactoryComponent<gtk::ListBox, SidebarInput> for List {
     type Command = ();
     type CommandOutput = ();
     type Input = ListInput;
     type Output = ListOutput;
     type InitParams = List;
-    type Root = gtk::Box;
     type Widgets = ListWidgets;
+
+    view! {
+        list_box = gtk::Box {
+            set_orientation: gtk::Orientation::Horizontal,
+            #[name = "icon"]
+            gtk::Image {
+                set_from_icon_name: Some(self.icon_name.as_ref().unwrap())
+            },
+            #[name = "name"]
+            gtk::Label {
+                set_halign: gtk::Align::Start,
+                set_hexpand: true,
+                set_text: self.display_name.as_str(),
+                set_margin_top: 10,
+                set_margin_bottom: 10,
+                set_margin_start: 15,
+                set_margin_end: 15,
+            },
+            #[name = "count"]
+            gtk::Label {
+                set_halign: gtk::Align::End,
+                set_css_classes: &["dim-label", "caption"],
+                #[watch]
+                set_text: self.count.to_string().as_str(),
+                set_margin_top: 10,
+                set_margin_bottom: 10,
+                set_margin_start: 15,
+                set_margin_end: 15,
+            }
+        }
+    }
 
     fn output_to_parent_msg(output: Self::Output) -> Option<SidebarInput> {
         Some(match output {
@@ -50,68 +75,30 @@ impl FactoryComponent<gtk::ListBox, SidebarInput> for List {
 
     fn init_model(
         params: Self::InitParams,
-        index: &DynamicIndex,
-        input: &Sender<Self::Input>,
-        output: &Sender<Self::Output>,
+        _index: &DynamicIndex,
+        _input: &Sender<Self::Input>,
+        _output: &Sender<Self::Output>,
     ) -> Self {
         params
     }
 
-    fn init_root() -> Self::Root {
-        view! {
-            list_box = &gtk::Box {
-                set_orientation: gtk::Orientation::Horizontal,
-            }
-        }
-        list_box
-    }
-
     fn init_widgets(
         &mut self,
-        index: &DynamicIndex,
+        _index: &DynamicIndex,
         root: &Self::Root,
         _returned_widget: &gtk::ListBoxRow,
-        input: &Sender<Self::Input>,
-        output: &Sender<Self::Output>,
+        _input: &Sender<Self::Input>,
+        _output: &Sender<Self::Output>,
     ) -> Self::Widgets {
-        view! {
-            icon = &gtk::Image {
-                set_from_icon_name: Some(self.icon_name.as_ref().unwrap())
-            }
-        }
-        view! {
-            name = &gtk::Label {
-                set_halign: gtk::Align::Start,
-                set_hexpand: true,
-                set_text: self.display_name.as_str(),
-                set_margin_top: 10,
-                set_margin_bottom: 10,
-                set_margin_start: 15,
-                set_margin_end: 15,
-            }
-        }
-        view! {
-            count = &gtk::Label {
-                set_halign: gtk::Align::End,
-                set_css_classes: &["dim-label", "caption"],
-                set_text: track!(model.changed(List::count()), self.count.to_string().as_str()),
-                set_margin_top: 10,
-                set_margin_bottom: 10,
-                set_margin_start: 15,
-                set_margin_end: 15,
-            }
-        }
-        root.append(&icon);
-        root.append(&name);
-        root.append(&count);
-        ListWidgets { icon, name, count }
+        let widgets = view_output!();
+        widgets
     }
 
     fn update(
         &mut self,
         message: Self::Input,
-        input: &Sender<Self::Input>,
-        output: &Sender<Self::Output>,
+        _input: &Sender<Self::Input>,
+        _output: &Sender<Self::Output>,
     ) -> Option<Self::Command> {
         match message {
             ListInput::Rename(name) => self.display_name = name,
@@ -125,18 +112,5 @@ impl FactoryComponent<gtk::ListBox, SidebarInput> for List {
             }
         }
         None
-    }
-
-    fn update_view(
-        &self,
-        widgets: &mut Self::Widgets,
-        input: &Sender<Self::Input>,
-        output: &Sender<Self::Output>,
-    ) {
-        widgets.name.set_text(self.display_name.as_str());
-        if let Some(icon) = &self.icon_name {
-            widgets.icon.set_from_icon_name(Some(icon.as_str()));
-        }
-        widgets.count.set_text(self.count.to_string().as_str());
     }
 }
