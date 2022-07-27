02478197159885e63f7125a611a1824cbbcae26e
Eduardo Flores <edfloreshz@gmail.com>
2022-07-26 23:43:00 -0700

    Fix constants

--- src/app/constants.rs
+++ src/app/constants.rs
@@ -1,6 +1,2 @@
 pub static VERSION: &str = "0.1.1";
 pub static APPLICATION_ID: &str = "dev.edfloreshz.Done";
-//pub static GETTEXT_PACKAGE: &str = "done";
-//pub static LOCALEDIR: &str = "/app/share/locale";
-//pub static PKGDATADIR: &str = "/app/share/done";
-//pub static PROFILE: &str = "default";
