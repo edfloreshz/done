e6ef8fdd75d57869819e3f191b7c739f0976bc4a
Eduardo Flores <edfloreshz@gmail.com>
2022-07-26 19:06:28 -0700

    Fix for rust_embed
    Added debug-embed feature and removed some constants.

--- src/app/constants.rs
+++ src/app/constants.rs
@@ -1,6 +1,6 @@
 pub static VERSION: &str = "0.1.0";
-pub static GETTEXT_PACKAGE: &str = "done";
-pub static LOCALEDIR: &str = "/app/share/locale";
 pub static APPLICATION_ID: &str = "dev.edfloreshz.Done";
-pub static PKGDATADIR: &str = "/app/share/done";
-pub static PROFILE: &str = "default";
+//pub static GETTEXT_PACKAGE: &str = "done";
+//pub static LOCALEDIR: &str = "/app/share/locale";
+//pub static PKGDATADIR: &str = "/app/share/done";
+//pub static PROFILE: &str = "default";
