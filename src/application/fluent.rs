fea4a18d6dceefbab69a42b7c03bd819d7faef5d
Eduardo Flores <edfloreshz@gmail.com>
2022-08-03 12:00:41 -0700

    Fluent translations moved

--- src/application/fluent.rs
+++ src/application/fluent.rs
@@ -8,7 +8,7 @@ use once_cell::sync::Lazy;
 use rust_embed::RustEmbed;
 
 #[derive(RustEmbed)]
-#[folder = "locale/i18n/"]
+#[folder = "i18n/"]
 struct Localizations;
 
 pub static LANGUAGE_LOADER: Lazy<FluentLanguageLoader> = Lazy::new(|| {
