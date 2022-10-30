c8da103f41a2faf656fc0ffd08536d5d4ddc8621
Eduardo Flores <edfloreshz@gmail.com>
2022-10-30 08:49:23 -0700

    gRPC example fixed

--- core/examples/grpc/build.rs
+++ core/examples/grpc/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../../proto/provider.proto")?;
+    tonic_build::compile_protos("provider.proto")?;
     Ok(())
 }
