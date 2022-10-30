54550f4cb7d98f3f8058f8c45d023103e8f926ba
Eduardo Flores <edfloreshz@gmail.com>
2022-10-30 10:55:33 -0700

    Added binaries for plugins to core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -6,10 +6,8 @@ extern crate diesel_migrations;
 use diesel_migrations::EmbeddedMigrations;
 
 pub mod models;
-pub mod traits;
 pub mod enums;
 mod data;
 mod schema;
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
-
