e45a254a54e8e035089353a36f5f9e1be8d39625
Eduardo Flores <edfloreshz@gmail.com>
2022-10-30 09:30:06 -0700

    Added remaining methods to the service file

--- core/examples/grpc/src/local.rs
+++ core/examples/grpc/src/local.rs
@@ -6,18 +6,71 @@ pub mod provider {
     tonic::include_proto!("provider");
 }
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:5123".parse()?;
+
+    let local_service = LocalService {
+        id: "Local Service".to_string(),
+        name: "Local".to_string(),
+        description: "Local tasks are stored here.".to_string(),
+        icon: "".to_string()
+    };
+
+    Server::builder()
+        .add_service(ProviderServer::new(local_service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
+
 #[derive(Debug, Default)]
 pub struct LocalService {
-    id: String
+    id: String,
+    name: String,
+    description: String,
+    icon: String,
 }
 
 #[tonic::async_trait]
 impl Provider for LocalService {
-    async fn get_id(&self, _request: Request<Void>) -> Result<Response<String>, Status> {
+    async fn get_id(
+        &self,
+        _request: Request<Void>,
+    ) -> Result<Response<String>, Status> {
         Ok(Response::new(self.id.clone()))
     }
 
-    async fn add_task(&self, request: Request<ProviderRequest>) -> Result<Response<ProviderResponse>, Status> {
+    async fn get_name(
+        &self,
+        _request: Request<Void>,
+    ) -> Result<Response<String>, Status> {
+        Ok(Response::new(self.name.clone()))
+    }
+
+    async fn get_description(
+        &self,
+        _request: Request<Void>,
+    ) -> Result<Response<String>, Status> {
+        Ok(Response::new(self.description.clone()))
+    }
+
+    async fn get_icon_name(
+        &self,
+        _request: Request<Void>,
+    ) -> Result<Response<String>, Status> {
+        Ok(Response::new(self.icon.clone()))
+    }
+
+    async fn read_all_tasks(
+        &self,
+        request: Request<ProviderRequest>,
+    ) -> Result<Response<ProviderResponse>, Status> {
+        todo!()
+    }
+
+    async fn create_task(&self, request: Request<ProviderRequest>) -> Result<Response<ProviderResponse>, Status> {
         println!("Local Service got a request: {:#?}", request);
 
         let req = request.into_inner();
@@ -28,20 +81,60 @@ impl Provider for LocalService {
         };
         Ok(Response::new(reply))
     }
-}
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:5123".parse()?;
+    async fn read_task(
+        &self,
+        request: Request<ProviderRequest>,
+    ) -> Result<Response<ProviderResponse>, Status> {
+        todo!()
+    }
 
-    let local_service = LocalService {
-        id: "Local Service".to_string()
-    };
+    async fn update_task(
+        &self,
+        request: Request<ProviderRequest>,
+    ) -> Result<Response<ProviderResponse>, Status> {
+        todo!()
+    }
 
-    Server::builder()
-        .add_service(ProviderServer::new(local_service))
-        .serve(addr)
-        .await?;
+    async fn delete_task(
+        &self,
+        request: Request<ProviderRequest>,
+    ) -> Result<Response<ProviderResponse>, Status> {
+        todo!()
+    }
 
-    Ok(())
+    async fn read_all_lists(
+        &self,
+        request: Request<ProviderRequest>,
+    ) -> Result<Response<ProviderResponse>, Status> {
+        todo!()
+    }
+
+    async fn create_list(
+        &self,
+        request: Request<ProviderRequest>,
+    ) -> Result<Response<ProviderResponse>, Status> {
+        todo!()
+    }
+
+    async fn read_list(
+        &self,
+        request: Request<ProviderRequest>,
+    ) -> Result<Response<ProviderResponse>, Status> {
+        todo!()
+    }
+
+    async fn update_list(
+        &self,
+        request: Request<ProviderRequest>,
+    ) -> Result<Response<ProviderResponse>, Status> {
+        todo!()
+    }
+
+    async fn delete_list(
+        &self,
+        request: Request<ProviderRequest>,
+    ) -> Result<Response<ProviderResponse>, Status> {
+        todo!()
+    }
 }
