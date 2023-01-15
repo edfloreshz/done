eb16168d9ff59a9f3fd71d6cf534796fc96ddbca
Eduardo Flores <edfloreshz@gmail.com>
2023-01-15 13:11:25 -0700

    Separating the welcome screen from the content

---
+++ src/widgets/components/welcome.rs
@@ -0,0 +1,59 @@
+use relm4::{SimpleComponent, ComponentSender, ComponentParts};
+use relm4::gtk::prelude::{OrientableExt, WidgetExt, BoxExt};
+use relm4::gtk;
+use relm4::RelmWidgetExt;
+
+use crate::fl;
+
+
+#[derive(Debug)]
+pub struct Welcome {
+
+}
+
+#[relm4::component(pub)]
+impl SimpleComponent for Welcome {
+    type Input = ();
+    type Output = ();
+    type Init = ();
+    type Widgets = WelcomeWidgets;
+
+    view! {
+        #[root]
+        gtk::Box {
+            gtk::CenterBox {
+                set_orientation: gtk::Orientation::Vertical,
+                set_halign: gtk::Align::Center,
+                set_valign: gtk::Align::Center,
+                #[wrap(Some)]
+                set_center_widget = &gtk::Box {
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_margin_all: 24,
+                    set_spacing: 24,
+                    gtk::Picture {
+                        set_resource: Some("/dev/edfloreshz/Done/icons/scalable/actions/paper-plane.png"),
+                        set_margin_all: 70
+                    },
+                    gtk::Label {
+                        set_css_classes: &["title-2", "accent"],
+                        set_text: fl!("select-list")
+                    },
+                    gtk::Label {
+                        set_text: fl!("tasks-here")
+                    }
+                }
+            }
+        }
+    }
+
+    fn init(
+            
+            _init: Self::Init,
+            root: &Self::Root,
+            _sender: ComponentSender<Self>,
+        ) -> ComponentParts<Self> {
+        let widgets =  view_output!();
+        let model = Welcome {};
+        ComponentParts { model, widgets }
+    }
+}
