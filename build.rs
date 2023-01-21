a8c51effe9613a41178a05f0adfc905979973f0b
Eduardo Flores <edfloreshz@gmail.com>
2023-01-21 11:12:22 -0700

    Meson build system

--- build.rs
+++ build.rs
@@ -1,7 +1,16 @@
+use std::{path::PathBuf, env};
+
 fn main() {
 	glib_build_tools::compile_resources(
 		"data/resources/",
 		"data/resources/resources.gresource.xml",
 		"resources.gresource",
 	);
+	let build_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    build_path.join("build");
+    let build_path = build_path.to_str().unwrap();
+
+    println!("cargo:rustc-link-lib=squid");
+    println!("cargo:rustc-link-search=native={}", build_path);
+    meson::build(".", build_path);
 }
