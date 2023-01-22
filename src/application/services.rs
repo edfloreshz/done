332c8673d05e4f4507cf620b5fc7548fef93f03d
Eduardo Flores <edfloreshz@gmail.com>
2023-01-22 16:47:48 -0700

    Better error handling
    - Modified plugin version to test

--- src/application/services.rs
+++ src/application/services.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 use super::plugin::Plugin;
 
 pub(crate) async fn init() -> Result<()> {
-	for plugin in Plugin::fetch_plugins().await.unwrap() {
+	for plugin in Plugin::get_plugins()? {
 		if !plugin.is_running() {
 			if let Err(e) = plugin.start() {
 				tracing::info!("{:?}", e);
