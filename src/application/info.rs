40416066e68d24fbc62132f1dc95f9806c46f0bb
Eduardo Flores <edfloreshz@gmail.com>
2023-01-22 23:08:37 -0700

    Bumped version

--- src/application/info.rs
+++ src/application/info.rs
@@ -5,6 +5,5 @@ pub const LOCALEDIR: &str = "/app/share/locale";
 pub const PKGDATADIR: &str = "/app/share/done";
 pub const PROFILE: &str = "Devel";
 #[allow(dead_code)]
-pub const RESOURCES_FILE: &str =
-	concat!("/app/share/done", "/resources.gresource");
-pub const VERSION: &str = "0.1.2-d5ac365";
+pub const RESOURCES_FILE: &str = concat!("/app/share/done", "/resources.gresource");
+pub const VERSION: &str = "0.1.2-9c66e5f";
