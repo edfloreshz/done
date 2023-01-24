67a09ef583c669c73e2aab9fcf9631ece9c30af2
Eduardo Flores <edfloreshz@gmail.com>
2023-01-24 10:47:16 -0700

    Better error handling

--- src/application/info.rs
+++ src/application/info.rs
@@ -1,10 +1,9 @@
-pub const APP_ID: &str = "dev.edfloreshz.Done.Devel";
+pub const APP_ID: &str = "dev.edfloreshz.Done";
 pub const GETTEXT_PACKAGE: &str = "done";
-pub const LOCALEDIR: &str = "/app/share/locale";
+pub const LOCALEDIR: &str = "/home/eduardo/.var/app/org.gnome.Builder/cache/gnome-builder/install/done/host/share/locale";
 #[allow(dead_code)]
-pub const PKGDATADIR: &str = "/app/share/done";
-pub const PROFILE: &str = "Devel";
+pub const PKGDATADIR: &str = "/home/eduardo/.var/app/org.gnome.Builder/cache/gnome-builder/install/done/host/share/done";
+pub const PROFILE: &str = "";
 #[allow(dead_code)]
-pub const RESOURCES_FILE: &str =
-	concat!("/app/share/done", "/resources.gresource");
-pub const VERSION: &str = "0.1.2-9c66e5f";
+pub const RESOURCES_FILE: &str = concat!("/home/eduardo/.var/app/org.gnome.Builder/cache/gnome-builder/install/done/host/share/done", "/resources.gresource");
+pub const VERSION: &str = "0.1.2";
